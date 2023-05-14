45ff2ab41db2bad10f92579bdeadb0765ec5f500
haridalavai <dalavai.hari@gmail.com>
2023-05-15 01:09:36 +0530

    update user db

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,13 +1,9 @@
 import { IsString, IsUUID, IsOptional } from 'class-validator';
 
 export class CreateUserDto {
-  @IsUUID()
-  id: string;
-
-  @IsString()
-  @IsOptional()
-  name: string;
-
   @IsString()
   email: string;
+
+  @IsString()
+  authId: string;
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -15,8 +15,6 @@ export class UserService {
         },
       });
 
-      console.log(existUser);
-
       if (existUser) {
         throw new BadRequestException('Email already exists');
       }
